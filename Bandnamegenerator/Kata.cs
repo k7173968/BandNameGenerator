5cb72d5a074d1075cb0d584a075a06060cc8d968
Dunk Hsieh Cheng-Lin <dunk.hsieh@titansoft.com.sg>
2017-09-20 13:02:08 +0800

    BandNameGenerator

---
+++ Bandnamegenerator/Kata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bandnamegenerator
+{
+    class Kata
+    {
+        public static string BandNameGenerator(string str)
+        {
+            if (str == null || str.Length==1)
+                return "";
+            if (Equals(str.First(), str.Last()))
+            {
+                return str.First().ToString().ToUpper() + str.Substring(1) + str.Substring(1);
+            }
+            else
+            {
+                return "The " + str.First().ToString().ToUpper() + str.Substring(1);
+            }
+        }
+    }
+}
---
+++ Bandnamegenerator/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Bandnamegenerator
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("",Kata.BandNameGenerator(null));
+        }
+        
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("", Kata.BandNameGenerator("a"));
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual("Tart", Kata.BandNameGenerator("tart"));
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual("Tartart", Kata.BandNameGenerator("tart"));
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Assert.AreEqual("Knife", Kata.BandNameGenerator("knife"));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.AreEqual("The Knife", Kata.BandNameGenerator("knife"));
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Assert.AreEqual("Sandlesandles", Kata.BandNameGenerator("sandles"));
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Assert.AreEqual("The Bed", Kata.BandNameGenerator("bed"));
+        }
+    }
+}
